733fa4c8feeafc75ad165e1ac1f4d46f35f9ac55
BennyBlanco88 <ben.langs@yahoo.com>
2023-12-09 09:50:07 -0800

    fixing bus to make sql work

--- server.js
+++ server.js
@@ -452,7 +452,7 @@ function viewEmployeesByManager() {
         // Function to view Employees by Department
         function viewEmployeesByDepartment() {
             const query =
-                "SELECT departments.department_name, employee.first_name, employee.last_name FROM employee INNER JOIN roles ON employee.role_id = roles.id = role.id INNER JOIN departments ON roles.department_id = departments.id ORDER BY departments.departments.department_name ASC";
+            "SELECT departments.department_name, employee.first_name, employee.last_name FROM employee INNER JOIN roles ON employee.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id ORDER BY departments.department_name ASC";
 
             connection.query(query, (err, res) => {
                 if (err) throw err;
